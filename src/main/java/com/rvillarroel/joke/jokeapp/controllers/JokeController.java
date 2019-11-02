5d556a0d8c6bb241bb7a347cae6d77881f68c55a
Ronny <ronaldovm.villarroel@gmail.com>
2019-11-02 14:59:17 -0400

    Class JokeController wasn't correct annotated

--- src/main/java/com/rvillarroel/joke/jokeapp/controllers/JokeController.java
+++ src/main/java/com/rvillarroel/joke/jokeapp/controllers/JokeController.java
@@ -1,10 +1,12 @@
-package controllers;
+package com.rvillarroel.joke.jokeapp.controllers;
 
 import com.rvillarroel.joke.jokeapp.services.JokeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class JokeController {
     private JokeService jokeService;
 
